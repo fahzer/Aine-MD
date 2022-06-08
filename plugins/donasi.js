f3e9bcfa48eb9c5a559e5da9cf7f609e5bc5d674
fahzer <107020771+fahzer@users.noreply.github.com>
2022-06-09 01:55:50 +0800

    Update donasi.js

--- plugins/donasi.js
+++ plugins/donasi.js
@@ -1,7 +1,7 @@
 let handler = async m => m.reply(`
 ╭─「 Donasi • Pulsa 」
-│ •  [0895-3303-79186]
-│ •  [0896-5436-0447]
+│ •  [0857-5418-0729]
+│ •  [0857-5418-0729]
 ╰────
 `.trim()) // Tambah sendiri kalo mau
 handler.help = ['donasi']
