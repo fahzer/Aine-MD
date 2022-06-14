738c2087ecc19749ed6c6d0b9e2cfbf3013fb4b4
fahzer <107020771+fahzer@users.noreply.github.com>
2022-06-14 12:12:57 +0800

    Update ainehi.js

--- plugins/ainehi.js
+++ plugins/ainehi.js
@@ -7,7 +7,7 @@ conn.sendFile(m.chat, helloaine, '', '', m, true)
 // await conn.sendMessage(m.chat, { audio: { url: helloaine }, mimetype: 'audio/mp4'}, m)
 }
 
-handler.customPrefix = /^(hi|hii|hiii|hi fahz|hii aine|hiii fahz|hy|halo|hallo|helo|hello|hy fahz|halo fahz|hallo fahz|p|helo fahz|hello fahz)$/i
+handler.customPrefix = /^(hi|hii|hiii|hi fahz|hii fahz|hiii fahz|hy|halo|hallo|helo|hello|hy fahz|halo fahz|hallo fahz|helo fahz|hello fahz)$/i
 handler.command = new RegExp
 
 handler.limit = true
